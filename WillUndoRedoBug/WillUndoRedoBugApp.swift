1b3e852f3514e052980a249f852071b78e0c8909
Ferenc Kiss <kiss.ferenc@me.com>
2023-04-03 07:31:52 +0200

    Initial Commit

---
+++ WillUndoRedoBug/WillUndoRedoBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  WillUndoRedoBugApp.swift
+//  WillUndoRedoBug
+//
+//  Created by Kiss Ferenc on 2023. 04. 03..
+//
+
+import SwiftUI
+
+@main
+struct WillUndoRedoBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
